34bff817fa36aaad78874575dadb7b18fab05d54
mandeepkakkar2003 <mandeepkakkar2003@gmail.com>
2024-06-25 14:02:09 +0530

    Updated front-end and backend configurations for deployment

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-const express=require ("express");
-const app=express();
-const path =require('path')
-const connectDB=require('./config/db');
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./config/db');
+
 connectDB();
+
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 
-const PORT=process.env.PORT || 3000;
+// Update this to your actual Netlify deployment URL
+app.use(cors({
+  origin: 'https://stupendous-lily-5e8714.netlify.app'
+}));
 
+const PORT = process.env.PORT || 3000;
 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
-
-
-app.listen(3000,()=>{
-    console.log("LISTENING ON PORT 3000");
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
